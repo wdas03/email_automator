96182d9c9e363add9675e6191ea061b955e95b24
William Das <williamhdas@gmail.com>
2023-09-12 15:54:29 -0400

    Send emails and reply to threads

--- gmail_api.py
+++ gmail_api.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from email.mime.multipart import MIMEMultipart
 
 import os.path
 
@@ -8,15 +9,21 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+from email.mime.text import MIMEText
+import base64
+
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly',
+          'https://www.googleapis.com/auth/gmail.send',
+          'https://www.googleapis.com/auth/gmail.modify']
+
 
 class GmailAPI:
     def __init__(self, reauthenticate=True):
         self.service = self.authenticate_user(reauthenticate)
 
     @staticmethod
-    def authenticate_user(reauthenticate=True):
+    def authenticate_user(reauthenticate):
         """Shows basic usage of the Gmail API.
         """
         creds = None
@@ -69,3 +76,70 @@ class GmailAPI:
 
         except HttpError as error:
             print(f'An error occurred: {error}')
+    
+    def create_message(self, sender, sender_name, to, subject, message_text):
+        """Create a MIME Text message with a custom sender name."""
+        msg = MIMEMultipart()
+        msg['to'] = to
+        msg['from'] = f"{sender_name} <{sender}>"
+        msg['subject'] = subject
+
+        msg.attach(MIMEText(message_text, 'plain'))
+
+        raw_message = base64.urlsafe_b64encode(msg.as_string().encode('utf-8'))
+        return {'raw': raw_message.decode('utf-8')}
+
+    def send_message(self, user_id, message):
+        """Send an email message."""
+        try:
+            message = self.service.users().messages().send(userId=user_id, body=message).execute()
+            print(f"Message Id: {message['id']}")
+            return message
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+
+    def send_individual_email(self, sender, to, subject, message_text):
+        """Send an individual email."""
+        message = self.create_message(sender, to, subject, message_text)
+        self.send_message("me", message)
+
+    def send_mass_email(self, sender, recipients, subject, message_text):
+        """Send a mass email to multiple recipients."""
+        for recipient in recipients:
+            self.send_individual_email(sender, recipient, subject, message_text)
+    
+    def create_reply_message(self, parent_message, sender, subject, message_text):
+        """Create a MIME Text message for replying to an existing thread."""
+        msg = MIMEMultipart()
+        msg['To'] = parent_message['from']
+        msg['From'] = sender
+        msg['Subject'] = subject
+        msg['In-Reply-To'] = parent_message['messageId']
+        msg['References'] = parent_message['messageId']
+
+        msg.attach(MIMEText(message_text, 'plain'))
+
+        raw_message = base64.urlsafe_b64encode(msg.as_string().encode('utf-8'))
+        return {'raw': raw_message.decode('utf-8'), 'threadId': parent_message['threadId']}
+
+    def reply_to_thread(self, thread_id, sender, subject, message_text):
+        """Reply to an existing thread."""
+        # Get the last message in the thread
+        thread_data = self.service.users().threads().get(userId='me', id=thread_id).execute()
+        last_message = thread_data['messages'][-1]
+        last_message_data = last_message['payload']['headers']
+
+        parent_message = {}
+        for header in last_message_data:
+            if header['name'] == 'From':
+                parent_message['from'] = header['value']
+            if header['name'] == 'Message-ID':
+                parent_message['messageId'] = header['value']
+
+        parent_message['threadId'] = thread_id
+
+        # Create the reply message
+        message = self.create_reply_message(parent_message, sender, subject, message_text)
+
+        # Send the message
+        self.send_message('me', message)
